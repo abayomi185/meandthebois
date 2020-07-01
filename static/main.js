bb1800a72754df3f738e000ff4ebe34641e08a2f
Yomi Ikuru(Windows) <captyomjnr@gmail.com>
2020-07-01 14:40:11 +0100

    Twitch Stream Test

--- static/main.js
+++ static/main.js
@@ -7,12 +7,24 @@ $(window).on('resize', function(){
     viewportHeight = ($(window).height() / 1.5);
 });
 
+function getQueryString() {
+    var queryStringKeyValue = window.parent.location.search.replace('?', '').split('&');
+    var qsJsonObject = {};
+    if (queryStringKeyValue != '') {
+        for (i = 0; i < queryStringKeyValue.length; i++) {
+            qsJsonObject[queryStringKeyValue[i].split('=')[0]] = queryStringKeyValue[i].split('=')[1];
+        }
+    }
+    return qsJsonObject;
+}
+
 var options = {
     width: viewportWidth,
     height: viewportHeight,
     video: "624030538",
+    theme: "dark",
     // only needed if your site is also embedded on embed.example.com and othersite.example.com 
-    parent: ["meandthebois.club", "yomitosh.com/meandthebois"]
+    parent: [getQueryString.name]
 };
 
 var player = new Twitch.Player("<player div ID>", options);
